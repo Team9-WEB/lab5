0cd4457a0a224ac9a7bdf4471080c4eed5d08472
MrZend <vlad04102002@gmail.com>
2021-11-04 13:26:09 +0200

    add: task4 report

--- Report/js/text_lab_info_5.js
+++ Report/js/text_lab_info_5.js
@@ -94,3 +94,132 @@ function itog_5() {
         <p style="text-align:justify;">На лабораторній роботі №5 ми придбали практичні навички роботи jQuery UI, додали до сайту  динамічні елементи цієї бібліотеки, плагін галереї UniteGallery, кнопки соціальних мереж, інформери та інше.</p>`
     changeText(text);
 }
+
+function fideo5(){
+    const text = `
+    <h1>ФотоСлайдер</h1>
+    <img src="lab_img/videoRes.png" >
+    <h2>HTML</h2>
+    <xmp style="text-align:justify; background-color:whitesmoke;">
+    <header>
+        <div id="videoslider">
+            <div data-type="youtube"
+                data-videoid="YvzjG19zhf0"
+                data-title="Unbelievable Beauty"
+                data-description="by Go Pro"></div>
+
+            <div data-type="youtube"
+                data-videoid="Rs3-OJBYKSY"
+                data-title="Italy"
+                data-description="by Go Pro"></div>
+
+            <div data-type="youtube"
+                data-videoid="lJp6BezFLCk"
+                data-title="Beauty"
+                data-description="by Go Pro"></div>
+
+            <div data-type="youtube"
+                data-videoid="lBJyaIR1mlw"
+                data-title="Iceland"
+                data-description="by Go Pro"></div>
+        </div>
+    </header>
+    </html>
+    </xmp>
+    <h2>Підключення JS</h2>
+    <xmp style="text-align:justify; background-color:whitesmoke;">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js" type="text/javascript"></script>
+    <script src='unitegallery/js/unitegallery.min.js' type='text/javascript'  ></script>
+    <link  href='unitegallery/css/unite-gallery.css' rel='stylesheet' type='text/css' />				
+    <script src='unitegallery/themes/slider/ug-theme-slider.js' type='text/javascript'></script>
+    <script src='unitegallery/themes/tiles/ug-theme-tiles.js' type='text/javascript'></script>
+    <script src="js/main.js"></script>
+    </xmp>
+    <h2>JS</h2>
+    <xmp style="text-align:justify; background-color:whitesmoke;">
+    jQuery("#videoslider").unitegallery({
+        gallery_theme: "slider",
+        gallery_width: 900,
+        gallery_height: 500,
+        slider_transition_speed:1000,
+    });
+    </xmp>
+    `;
+    changeText(text);
+}
+
+function foto5(){
+    const text = `
+    <h1>Галерея</h1>
+    <img src="lab_img/galleryRes.png" >
+    <h2>HTML</h2>
+    <xmp style="text-align:justify; background-color:whitesmoke;">
+    <main>
+        <div id="gallery">
+            <img src="https://storge.pic2.me/cm/3840x2160/427/585cd40213f5a.jpg" alt="..."/>
+            <img src="https://storge.pic2.me/c/1360x800/555/57753010826d4.jpg" alt="..."/>
+            <img src="https://funart.pro/uploads/posts/2021-03/1617054432_6-p-oboi-priroda-4k-6.jpg" alt="..."/>
+            <img src="https://img5.goodfon.ru/wallpaper/nbig/e/6b/les-nebo-oblaka-peizazh-gory-priroda-ozero-otrazhenie-bere-4.jpg" alt="..."/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRtOXNkHWO0ZC0dkPGZ7gL6mk10ajgldVGqAlob8R9tO3UeyzG-qvbHnaD5cQud32RoEA&usqp=CAU" alt="..."/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3FXjvJHNO1vn3VLQFrjxuKkkDZhgRHf8uAQ&usqp=CAU" alt="..."/>
+            <img src="https://i.artfile.ru/2048x1152_929297_[www.ArtFile.ru].jpg" alt="..."/>
+        </div>
+    </main>
+    </xmp>
+    <h2>JS</h2>
+    <xmp style="text-align:justify; background-color:whitesmoke;">
+    jQuery("#gallery").unitegallery({
+        gallery_theme: "tiles",
+        tiles_space_between_cols:25,
+	    tiles_justified_space_between:25,
+	    tiles_col_width:220,
+	    tile_enable_shadow:true,
+	    theme_gallery_padding:25,
+	    tiles_type: "justified",
+    });
+    </xmp>
+    `;
+    changeText(text);
+}
+
+function slider5(){
+    const text = `
+    <h1>Галерея</h1>
+    <img src="lab_img/sliderRes.png" >
+    <h2>HTML</h2>
+    <xmp style="text-align:justify; background-color:whitesmoke;">
+    <div id="slider1">
+            <img src="https://images.hdqwalls.com/wallpapers/cyberpunk-city-world-map-4k-vc.jpg" alt="..."/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBz6ieaZzBguL7f9zS7-v-OVILMLpxzKN1-mXqCHWkSEntylWk83Eg8zxdNQzLfVvxkUo&usqp=CAU" alt="..."/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLS4HxgWVjOFAMJk-K1cTCoINv2GNoTvx0MV0s3jLdaHTuR7gPCj6HxFaHxGCzPwtw_0M&usqp=CAU" alt="..."/>
+    </div>
+    </xmp>
+    <h2>JS</h2>
+    <xmp style="text-align:justify; background-color:whitesmoke;">
+    jQuery("#slider1").unitegallery({
+        gallery_theme: "slider",
+        gallery_width: 450,
+        gallery_height: 300,
+        gallery_play_interval: 9000,
+        slider_transition_speed:1000,
+    });
+
+    jQuery("#slider2").unitegallery({
+        gallery_theme: "slider",
+        gallery_width: 450,
+        gallery_height: 300,
+        gallery_play_interval: 8000,
+        slider_transition_speed:1000,
+    });
+
+    jQuery("#slider3").unitegallery({
+        gallery_theme: "slider",
+        gallery_width: 450,
+        gallery_height: 300,
+        gallery_play_interval: 10000,
+        slider_transition_speed:1000,
+    });
+    </xmp>
+    `;
+    changeText(text);
+}
