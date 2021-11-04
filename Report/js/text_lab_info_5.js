d45359beb15d0315894b194c6ae81a3890d15db7
Denys-0609 <72230091+Denys-0609@users.noreply.github.com>
2021-11-04 12:35:52 +0200

    report

---
+++ Report/js/text_lab_info_5.js
@@ -0,0 +1,81 @@
+function meta_5() {
+    const text = `<h1>Тема, Мета</h1>
+        <p>Тема : ВИКОРИСТАННЯ В СЦЕНАРІЯХ JAVASCRIPT БІБЛІОТЕК КРОС-БРАУЗЕРНИХ ІНТЕРФЕЙСІВ МЕТОДІВ DOM.  БІБЛІОТЕКА JQUERY UI.ЗАСОБИ НАЛАГОДЖЕННЯ І ТЕСТУВАННЯ СЦЕНАРІЇВ JAVASCRIPT.</p>
+        <p>Мета : придбати практичні навички роботи jQuery UI, вміти додавати до сайту  динамічні елементи цієї бібліотеки, плагін галереї UniteGallery, кнопки соціальних мереж, інформери та інше.</p>`;
+    changeText(text);
+}
+
+function dinamic5() {
+    const text = `<h1>Динамічні елементи 3 пункту</h1>
+        <h2>Завдання:</h2>
+        <img src="lab_img/lab5_img/task3.png">
+        <hr>
+        <h2>Tabs - організація вкладок на сторінці </h2>
+        <div style="text-align: left">
+            <h2>Результат:</h2>
+            <img src="lab_img/lab5_img/result_1.1.png" >
+            <h2>Код:</h2>
+            <img src="lab_img/lab6_img/dinamic/code1.1.png">
+            <img src="lab_img/lab6_img/dinamic/code1.2.png">
+        </div>
+        <hr>
+        <h2>Slider - вибір числа за допомогою повзунка. </h2>
+        <div style="text-align: left">
+            <h2>Результат:</h2>
+            <img src="lab_img/lab5_img/result_1.2.png" >
+            <h2>Код:</h2>
+            <img src="lab_img/lab6_img/dinamic/code2.1.png">
+            <img src="lab_img/lab6_img/dinamic/code2.2.png">
+        </div>
+        <hr>
+        <h2>Datepicker - вибір дати з календаря </h2>
+        <div style="text-align: left">
+            <h2>Результат:</h2>
+            <img src="lab_img/lab5_img/result_1.3.png" > 
+            </div>
+            <h2>Код:</h2>
+            <img src="lab_img/lab6_img/dinamic/code3.1.png">
+            <img src="lab_img/lab6_img/dinamic/code3.2.png">
+        </div>
+`;
+
+    changeText(text);
+}
+
+function buttons5() {
+    const text = `<h1>Кнопки соціальних мереж</h1>
+        <h2>Завдання:</h2>
+        <img src="lab_img/lab5_img/task5.png">
+        <hr>
+        <div style="text-align: left">
+            <h2>Результат:</h2>
+            <img src="lab_img/lab5_img/result_5.png" >
+            <h2>Код:</h2>
+            <img src="lab_img/lab5_img/code_5.png">
+        </div>
+`;
+
+    changeText(text);
+}
+
+function task7() {
+    const text = `<h1>Плагін Vide</h1>
+        <h2>Завдання:</h2>
+        <img src="lab_img/lab5_img/task7.png">
+        <hr>
+        <div style="text-align: left">
+            <h2>Результат:</h2>
+            <img src="lab_img/lab5_img/result_7.png" >
+            <h2>Код:</h2>
+            <img src="lab_img/lab5_img/code_7.png">
+        </div>
+`;
+
+    changeText(text);
+}
+
+function itog_5() {
+    const text = `<h1>Висновок</h1>
+        <p style="text-align:justify;">На лабораторній роботі №5 ми придбали практичні навички роботи jQuery UI, додали до сайту  динамічні елементи цієї бібліотеки, плагін галереї UniteGallery, кнопки соціальних мереж, інформери та інше.</p>`
+    changeText(text);
+}
